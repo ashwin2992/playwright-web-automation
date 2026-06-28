d2f55ebf407d64dc842dd5d163fda1a9c0898b07
Ashwin <ashwinnetalkar@gmail.com>
2026-06-28 22:32:00 +0530

    config&yml update

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     
-    headless:false,
+    headless:true,
     trace: 'retain-on-failure',
     screenshot: "on",
     video: "retain-on-failure"
